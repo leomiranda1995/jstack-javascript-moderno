d972de7d311eb36077d12210ccd2a8af8aaf0914
Leonardo da Silva Miranda <leomiranda1995@gmail.com>
2022-03-08 14:14:30 -0300

    .some e .every

--- 09-hof.js
+++ 09-hof.js
@@ -5,9 +5,17 @@ const array = [
 ];
 
 /** .find -> retorna o primeiro em que a nossa condição retorna true */
-const find = array.find((product) => (product.name > 1000));
+const find = array.find((product) => product.name > 1000);
 // console.log({ find });
 
 /** .findIndex -> retorna em qual posição do nosso array o elemento está quando a nossa condição retorna true */
-const findIndex = array.findIndex((product) => (product.name ===  'Magic Mouse'));
-console.log({ findIndex });
+const findIndex = array.findIndex((product) => product.name ===  'Magic Mouse');
+// console.log({ findIndex });
+
+/** .some -> verifica se algum elemento do nosso array bate com a condição */
+const some = array.some((product) => product.price < 1000);
+// console.log({ some });
+
+/** .every -> verifica se todos os elementos cumprem a regra da condição */
+const every = array.every((product) => product.price >= 1000);
+// console.log(every);
