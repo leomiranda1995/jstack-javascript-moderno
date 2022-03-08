e94a5133395a7f691651ad627c70bcfeb65e499a
Leonardo da Silva Miranda <leomiranda1995@gmail.com>
2022-03-08 14:28:27 -0300

    .filter

--- 09-hof.js
+++ 09-hof.js
@@ -26,3 +26,7 @@ const map = array.map((product) => ({
   subtotal: product.quantity * product.price,
 }));
 // console.log({ map });
+
+/** .filter -> filtra os elementos do nosso array que atendem a uma condição definida */
+const filter = array.filter((product) => product.quantity > 1);
+// console.log(filter);
